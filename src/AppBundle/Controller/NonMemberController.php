4cf0668af67c111a0b6fdabcd605fbcdfc0ac67b
Eduard Rotärmel <eduard.rotaermel@st.ovgu.de>
2016-03-07 14:45:45 +0100

    h

--- src/AppBundle/Controller/NonMemberController.php
+++ src/AppBundle/Controller/NonMemberController.php
@@ -4,26 +4,29 @@ namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Request;
+
+use AppBundle\Entity\Nichtmitglieder\Nonmember;
 
 class NonMemberController extends Controller {
     /**
-     * @Route("/nichtmitglieder/{letter}", defaults={"letter"="A"}, name="nonmember_home", requirements={"letter": "[A-Z]"})
+     * @Route("/nichtmitglieder/{letter}/{info}", defaults={"letter"="A", "info"=null}, name="nonmember_home", requirements={"info": "null", "letter": "[A-Z]"})
     */    
-    public function indexAction($letter) {   
+    public function indexAction($letter, $info) {   
   
         
     $repository = $this->getDoctrine()
     ->getRepository('AppBundle:Nichtmitglieder\Nonmember');
-         
+    
+    $qb=$repository->createQueryBuilder('n');
+    
         return $this->render(
         'Nicht_Mitglieder/nonmember.html.twig',
-        array(
-            'info' => NULL,
+        ['info' => $info,
+            'colorclass' => "bluetheader",
             'cletter' => $letter,
             'tabledata' => NULL, 
             'path' => 'nonmember_home'
-            ));
+            ]);
     }     
 }
 
