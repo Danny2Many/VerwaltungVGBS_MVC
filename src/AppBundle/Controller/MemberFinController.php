bb464dff1612dabc7ffa236d101cf5f1181c672b
Danny Görner <danny.goerner@gmail.com>
2016-03-07 14:22:13 +0100

    07032016

--- src/AppBundle/Controller/MemberFinController.php
+++ src/AppBundle/Controller/MemberFinController.php
@@ -7,8 +7,82 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use AppBundle\Form\Type\SearchType;
 use Symfony\Component\HttpFoundation\Request;
-
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 class MemberFinController extends Controller{
+    
+    /**
+     * @Route("/mitglieder/finanzen/{year}/{halfyear}", defaults={"year"=2016,"halfyear"=1}, name="member_finsearch", requirements={ "year": "[1-9][0-9]{3}", "halfyear": "1|2"})
+     * @Method({"GET"})
+     */
+    public function searchmemberfinAction(Request $request, $year, $halfyear){
+        
+    //query alle MemFinYears für die Dropdownlist
+        $finyearrepository = $this->getDoctrine()->getRepository('AppBundle:MemFinYear');
+        $memrepository = $this->getDoctrine()->getRepository('AppBundle:Member');
+        
+        $finyears=$finyearrepository->findAll();
+        
+        
+        
+        $qb = $memrepository->createQueryBuilder('m');
+        
+        $qb->Join('m.yearinfo', 'i')
+           
+           ->where('i.year = 2016');
+           
+        $pathhalfyear1=$request->getPathInfo();
+        
+        $choices=array('Mitgliedsnr.' => 'memid',
+        'Name' => 'lastname',
+        'Vorname' => 'firstname',
+        'Strasse' => 'streetaddress',
+        );
+     
+ 
+    $searchform = $this->createForm(SearchType::class, null, array('choices' => $choices, 'action' => $this->generateUrl('member_finsearch')));
+
+        $searchform->handleRequest($request);
+    
+//    $searchval=$request->query->get('search')['searchfield'];
+//    $searchcol=$request->query->get('search')['Spalte'];
+    
+    $searchval=$searchform->get('searchfield')->getData();
+    $searchcol=$searchform->get('Spalte')->getData();
+    
+    
+    $query=$qb->where($qb->expr()->like('m.'.$searchcol, ':member'))
+                   ->setParameter('member','%'.$searchval.'%')
+                   ->getQuery();
+    
+    $memberfinlist=$query->getResult();
+    
+     $disabled='';
+     
+
+    
+        return $this->render(
+        'Mitglieder/memberfin.html.twig',
+        array(
+            'tabledata' => $memberfinlist,
+            'colorclass' => "bluetheader",
+            'searchform' => $searchform->createView(),
+            'disabled' => $disabled,
+            
+            'cletter' => null,
+            'path' => 'member_fin',
+            'finyears' => $finyears,
+            'year' => $year,
+            'halfyear' => $halfyear,
+            'pathhalfyear1' => $pathhalfyear1,
+            'pathhalfyear2' => $pathhalfyear1
+            
+         
+            ));
+    }
+    
+    
+    
+    
     
     
     /**
@@ -30,23 +104,7 @@ class MemberFinController extends Controller{
            
            ->where('i.year = 2016');
            
-        $monthsfirsthalfyear=['Januar', 'Februar', 'März', 'April', 'Mai', 'Juni'];
-        $monthssecondhalfyear=['Juli', 'August', 'September', 'Oktober', 'November', 'Dezember'];
-        
-        
-        
-        switch($halfyear){
-            case 1:
-                $monthshalfyear=$monthsfirsthalfyear;
-                break;
-            case 2:
-                $monthshalfyear=$monthssecondhalfyear;
-        }
-
-        foreach ($monthshalfyear as $month){
-            $qb->orWhere('md.month='.$month);
-            
-        }
+   
         
         $choices=array('Mitgliedsnr.' => 'memid',
         'Name' => 'lastname',
@@ -59,27 +117,7 @@ class MemberFinController extends Controller{
 
         $searchform->handleRequest($request);
     
-    
-    
-    if($searchform->isSubmitted() && $searchform->isValid()){
-    $letter=null;   
-    $searchval=$request->query->get('search')['searchfield'];
-    $searchcol=$request->query->get('search')['Spalte'];
-    
-    
-    
-    
-    $query=$qb->where($qb->expr()->like('m.'.$searchcol, ':member'))
-                   ->setParameter('member','%'.$searchval.'%')
-                   ->getQuery();
-    
-    $memberfinlist=$query->getResult();
-    
-     $disabled='';
-     
-    }else{
-        
-        
+  
         $query=$qb->where($qb->expr()->like('m.lastname', ':letter'))
                    ->setParameter('letter',$letter.'%');
                    
@@ -99,14 +137,11 @@ class MemberFinController extends Controller{
             break;
         }
         
-        $query=$query->getQuery()->getResult();
+        $memberfinlist=$query->getQuery()->getResult();
         
-        $memberfinlist=$query;
+       
         $disabled='disabled';
-    }
-    
-    
-    
+   
         return $this->render(
         'Mitglieder/memberfin.html.twig',
         array(
@@ -120,8 +155,12 @@ class MemberFinController extends Controller{
             'finyears' => $finyears,
             'year' => $year,
             'halfyear' => $halfyear,
+            'pathhalfyear1' => null
             
          
             ));
     }
+    
+    
+    
 }
