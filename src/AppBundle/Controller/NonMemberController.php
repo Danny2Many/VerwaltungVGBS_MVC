403a0a64bbfba354f31b168e6513b9de5e2e3e3e
Eduard Rotärmel <eduard.rotaermel@st.ovgu.de>
2016-03-10 15:33:41 +0100

    Donnerstag 15:30

--- src/AppBundle/Controller/NonMemberController.php
+++ src/AppBundle/Controller/NonMemberController.php
@@ -91,9 +91,9 @@ class NonMemberController extends Controller {
         
         $nonmember = new Nonmember ();
        // $phonenumber = new NonMemPhoneNumber();
-                $section = new Section ();
+               $section = new Section ();
         
-          $nonmember->addSection($section);
+          //$nonmember->addSection($section);
 //        $nonmember->addPhonenumber($phonenumber);
         
         $addnonmemform = $this->createForm(AddNonMemberType::class, $nonmember);
