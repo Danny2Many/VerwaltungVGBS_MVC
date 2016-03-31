78584ae3ed270f5c0bc5c72c10b69ab55dfc9a88
Danny Görner <danny.goerner@gmail.com>
2016-03-31 21:32:21 +0200

    indexm

--- src/AppBundle/Services/IndexManager.php
+++ src/AppBundle/Services/IndexManager.php
@@ -3,46 +3,63 @@
 
 class IndexManager{
   
-    protected $entitypath;
+    protected $entityname;
     
     protected $em;
     
-    public function __construct($entitypath,$entityManager) {
-      $this->entitypath=$entitypath;
+    public function __construct($entityname,$entityManager) {
+      $this->entityname=$entityname;
       $this->em=$entityManager;
     }
     
     public function add($number=1){
-        $qb=  $this->em->createQueryBuilder();
-        $qb ->select('ditto')
-            ->from($this->entitypath, 'ditto')
-            ->where('index=:tablename')
-            ->setParameter('tablename', $this->getTableName());
         
         
-        $index=$qb->getQuery()->getSingleResult();
+        $index=getIndexEntity()->setIndex($index->getCurrentIndex()+$number);
         
-        $index->setIndex($index->getIndex()+$number);
+        $this->em->persist($index);
+        $this->em->push($index);
+        
+        
+    }
+    
+    
+    public function remove($number=1){
+        
+        
+        $index=getIndexEntity()->setIndex($index->getCurrentIndex()-$number);
         
         $this->em->persist($index);
         $this->em->push($index);
         
     }
     
+    
     public function getCurrentIndex(){
-        $qb=  $this->em->createQueryBuilder();
-        $qb->select('index')
-                 ->from($this->entitypath)
-                 ->where('index=:tablename')
-                 ->setParameter('tablename', $this->getTableName());
-        $index=$this->qb->getQuery()->getSingleResult();
-        return $index->getIndex();
+        
+        return $this->getIndexEntity()->getIndex();
     }
     
+    
+    
     public function getTableName(){
-        $splitpath=  explode("/", $this->entitypath);
+        
           
         
-        return end($splitpath);
+        return $this->entityname;
+    }
+    
+    
+    public function getIndexEntity(){
+        $qb=  $this->em->createQueryBuilder();
+        $qb ->select('ditto')
+            ->from('AppBundle:Indices', 'ditto')
+            ->where('index=:tablename')
+            ->setParameter('tablename', $this->entityname);
+        
+        
+        $index=$qb->getQuery()->getSingleResult();
+        
+        return $index;
     }
 }
