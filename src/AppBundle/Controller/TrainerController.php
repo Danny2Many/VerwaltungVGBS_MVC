3ac2ef06c6a9b25644f4742cb624aef6a34e0578
Peer Kokai <peerkokai@mail.de>
2016-04-15 09:38:03 +0200

    querry change

--- src/AppBundle/Controller/TrainerController.php
+++ src/AppBundle/Controller/TrainerController.php
@@ -49,16 +49,18 @@ class TrainerController extends Controller
     
         $qb= [];
         foreach($dependencies as $dependent => $idprefix){
-                 $qb[$dependent] = $doctrine->getRepository('AppBundle:'.$dependent)->createQueryBuilder('ditto');
 
             $qb[$dependent.'sub'] = $doctrine->getRepository('AppBundle:'.$dependent)->createQueryBuilder('dittosub');
             $qb[$dependent.'sub']->select($qb[$dependent.'sub']->expr()->max('dittosub.validfrom'))
-                    ->where('dittosub.'.$idprefix.'id=ditto.'.$idprefix.'id')
-                    ->andWhere('ditto.validfrom<='.$now)
-                    ->andWhere('ditto.validto>'.$now)
+                    ->where('dittosub.'.$idprefix.'id=dittosub.'.$idprefix.'id')
+                    ->andWhere('dittosub.validfrom<='.$now)
+                    ->andWhere('dittosub.validto>'.$now)
+                    
                     ;
             
+            $qb[$dependent] = $doctrine->getRepository('AppBundle:'.$dependent)->createQueryBuilder('ditto');
             $qb[$dependent]->where('ditto.validfrom=('.$qb[$dependent.'sub']->getDQL().')')
+                    
        
                     ;   
     }
