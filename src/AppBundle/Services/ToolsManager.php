73b4689157ccd03cb653a24700618d9eb013a958
Danny Görner <danny.goerner@gmail.com>
2017-08-27 19:40:50 +0200

    Added some comments in member.html and ToolsManager.php

--- src/AppBundle/Services/ToolsManager.php
+++ src/AppBundle/Services/ToolsManager.php
@@ -6,24 +6,27 @@ namespace AppBundle\Services;
 use Doctrine\Common\Collections\ArrayCollection;
 
 
-
+//Manages all written functions
 class ToolsManager {
 
-    public $manager;
+    //the entitymanager to persist and delete object to and from the database
+    public $entitymanager;
     
-    public function __construct($manager) {
-        $this->manager=$manager;
+    public function __construct($entitymanager) {
+        $this->entitymanager=$entitymanager;
     }
    
+    //Deletes objects of the $origenalArrayColl from the database which arent in the $submittedArrayColl
     public function sortOutRemoved($origenalArrayColl, $submittedArrayColl){
         foreach ($origenalArrayColl as $o) 
             {
             if (false === $submittedArrayColl->contains($o)) {
-                $this->manager->remove($o);
+                $this->entitymanager->remove($o);
             }
         } 
     }
     
+    //Copies all elements of the ArrayCollection $arrayCollToCopy to $copiedArrayColl and returns it
     public function copyArrayCollection($arrayCollToCopy){
         $copiedArrayColl = new ArrayCollection();
         foreach ($arrayCollToCopy as $element) {
